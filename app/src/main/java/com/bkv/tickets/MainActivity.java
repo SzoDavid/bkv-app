145f800bfcba6197917e1430327086a61d2cd651
SzoDavid <szobonya.david@gmail.com>
2024-05-06 20:31:02 +0200

    Initial commit

---
+++ app/src/main/java/com/bkv/tickets/MainActivity.java
@@ -0,0 +1,41 @@
+package com.bkv.tickets;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    private EditText emailET;
+    private EditText passwordET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        emailET = findViewById(R.id.emailEditText);
+        passwordET = findViewById(R.id.passwordEditText);
+    }
+
+    public void loginOnClick(View view) {
+        String email = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        Log.i(LOG_TAG, String.format("Bejelentkezett: %s ; %s", email, password));
+    }
+}
